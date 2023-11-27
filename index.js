7deb6f9486ab05f242768c2b2befec3e601d76db
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-11-27 08:56:21 +0600

    connect server with mongo db

--- index.js
+++ index.js
@@ -17,6 +17,34 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// connecting uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gwehrjf.mongodb.net/?retryWrites=true&w=majority`;
+
+// db connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    // confirm server connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Nexify server is running...");
 });
